82e1cc7c46bbdc16d304d8876b1ed0d80fa27788
Michael Hines <michael.hines@yale.edu>
2003-11-20 15:03:04 +0000

    upgrade to ltmain.sh (GNU libtool) 1.5a (1.1337 2003/11/19 17:58:39)
    git-svn-id: http://www.neuron.yale.edu/svn/neuron/iv/trunk@1352 a16e031d-7b14-0410-9afa-d909082ea83f

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # how to build from just the cvs sources
 
+if test ! -f "ltmain.sh" ; then
+	libtoolize -c -f #creates ltmain.sh config.guess config.sub libtool.m4
+fi
+        
 aclocal
 autoheader
 autoconf
