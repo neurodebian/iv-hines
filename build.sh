43a76a0b3681eae843f37b2631961549fb266ec1
Michael Hines <michael.hines@yale.edu>
2007-09-14 11:07:14 +0000

    svn merge -r 1834:1851 svn+ssh://hines@www.neuron.yale.edu/home/svnroot/neuron/iv/branches/cygso mswin creates cygIVhines-3.dll instead of statically linked library.
    git-svn-id: http://www.neuron.yale.edu/svn/neuron/iv/trunk@1854 a16e031d-7b14-0410-9afa-d909082ea83f

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ else
 	libtoolize -c -f
 fi
     
+#fix ltmain for cygwin to allow dlls
+sed '/undefined on the libtool link/,/allow_undefined/s/allow_undefined=yes/allow_undefined=no/' ltmain.sh > temp
+mv temp ltmain.sh
+chmod 755 ltmain.sh
+
 aclocal
 autoheader
 autoconf
