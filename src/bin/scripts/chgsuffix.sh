8566b413d2ec36afaa6da67c1140ec225e5daaed
Yaroslav Halchenko <debian@onerussian.com>
2010-10-12 13:22:35 -0400

    other removals

--- src/bin/scripts/chgsuffix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#set -x
-if [ "$1" \!= "$2" ]; then
-    # recursively change suffix in $1 for suffix in $2
-    find . -type f -name "*.$1" -print -o -type d -name TIFF -prune | \
-    sed "s/^\(.*\)$1/mv \1$1 \1$2" | \
-    sh
-fi
